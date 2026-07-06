8e27d5471c6e3dc95cd387e16ee3788d7fe6130d
HungNguyen254 <nguyenhung2540206@gmail.com>
2026-07-06 19:16:30 +0700

    Update MiniprojectSs9.py

--- MiniprojectSs9.py
+++ MiniprojectSs9.py
@@ -4,14 +4,22 @@ from datetime import datetime
 app = FastAPI()
 tasks_db = [
     {
-        "id": 1,
-        "title": "Learn FastAPI",
-        "description": "Study CRUD",
-        "assignee": "Hung",
-        "priority": 2,
+        "id": 1, 
+        "title": "Thiet ke database Shop AI", 
+        "description": "Xay dung bang va toi uu index", 
+        "assignee": "QuyDev", 
+        "priority": 1, 
         "status": "todo",
-        "created_at": "2026-07-06 10:00",
-        "internal_notes": "Only Admin"
+        "created_at": "2026-07-01T09:00:00Z"
+    },
+    {
+        "id": 2, 
+        "title": "Code bo API Authen", 
+        "description": "Trien khai filter verify JWT token", 
+        "assignee": "FixerQ", 
+        "priority": 2, 
+        "status": "done",
+        "created_at": "2026-07-01T10:00:00Z"
     }
 ]
 class TaskCreate(BaseModel):
